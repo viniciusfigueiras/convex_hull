b7221446a18d7695d907172fbc44e0ae3c0635f1
viniciusfigueiras <viniciusfifer@gmail.com>
2023-10-24 14:49:14 -0300

    swap

--- convex_hull.c
+++ convex_hull.c
@@ -21,9 +21,11 @@ typedef struct points {
 } points;
 
 int main(int argc, char *argv[]) {
-  int i;
+  int i, lo, hi;
   time_t t;
   points p_points[size];
+  lo = 0;
+  hi = size-1;
   
   srand((unsigned) time(&t));
   
@@ -32,17 +34,21 @@ int main(int argc, char *argv[]) {
     p_points[i].y = rand() % 20;
     printf(" point %d coords are = (%d %d)\n", i, p_points[i].x, p_points[i].y);
   }
+   
+  quicksort(p_points, lo, hi);
 
   return 0;
 }
 
 void quicksort(points *A ,int lo, int hi) {
   // Ensure indices are in correct order
-  if(lo >= hi || lo < 0)
-    return(NULL);
-    
+  if(lo >= hi || lo < 0) {
+    print("Indexes error");
+    exit(1);
+  }
+  
   // Partition array and get the pivot index
-  points* p = partition(A, lo, hi);
+  int p = partition(A, lo, hi);
       
   // Sort the two partitions
   quicksort(A, lo, p - 1); // Left side of pivot
@@ -51,24 +57,27 @@ void quicksort(points *A ,int lo, int hi) {
 
 // Simple selection of pivot
 int selection(lo, hi) {
-
     int mid = (lo + hi)/2;
     return(mid);
 }
 
+void swap(points *A, int a, int b){
+   int temp = *A[a];
+   *A[a] = *A[b];
+   *A[b] = temp;
+}
+
 // Divides array into two partitions
-int partition(points* A, int lo, int hi) { 
-  
-  int mid, pivot, i;
-  
+int partition(points *A, int lo, int hi) { 
+  int mid, pivot, i; 
   mid = selection(lo, hi);
-  if(A[mid] < A[lo]);
-    swap(A[lo], A[mid]);
-  if(A[hi] < A[lo]);
-    swap(A[lo], A[hi]);
-  if(A[mid] < A[hi]);
-    swap(A[mid], A[hi]);
-  pivot = A[hi];
+
+  if(A[mid] < A[lo])
+    swap(A, lo, mid);
+  if(A[hi] < A[lo])
+    swap(A, lo, hi);
+  if(A[mid] < A[hi])
+    swap(A, mid, hi);
 
   // Temporary pivot index
   i = lo - 1;
@@ -79,12 +88,12 @@ int partition(points* A, int lo, int hi) {
       // Move the temporary pivot index forward
       i = i + 1;
       // Swap the current element with the element at the temporary pivot index
-      swap(A[i], A[j]);
+      swap(A, i, j);
     }
   }
   // Move the pivot element to the correct pivot position (between the smaller and larger elements)
   i = i + 1;
-  swap(A[i], A[hi]);
+  swap(A, i, hi);
   return i;
 }
 
